773458135d0e5c2358180d484dfedd99b8018175
mani9896 <shankar.money08@gmail.com>
2021-06-25 16:44:27 +0530

    basic setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+
+
+app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is up on ${PORT}`);
+});
